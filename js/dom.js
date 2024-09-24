8899c31f2b4eb7c4dfd7b4d6edb781ef92fd9115
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-09-24 12:45:16 +0600

    dom bas

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,18 @@
-console.log('moduntain liza')
+const sectionList = document.getElementById('fristSec');
+const ul = document.createElement('ul');
+const li = document.createElement('li');
+li.innerText='biriyani';
+ul.appendChild(li);
+
+sectionList.appendChild(ul);
+
+
+sectionList.innerHTML=`
+<h1>dom dere baba</h1>
+    <ul>
+    <li>cheken biriyani</li>
+    <li>gorur manso</li>
+    <li>khasi</li>
+    <li>boilar</li>
+    </ul>
+`
